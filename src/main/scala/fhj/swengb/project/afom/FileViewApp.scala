4a1022c4f2de4b3cf5c380f1d867701786a942db
Lionade <steven95@gmx.at>
2016-01-06 14:35:01 +0100

    window test

--- src/main/scala/fhj/swengb/project/afom/FileViewApp.scala
+++ src/main/scala/fhj/swengb/project/afom/FileViewApp.scala
@@ -1,8 +1,33 @@
 package fhj.swengb.project.afom
 
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.{Scene, Parent}
+import javafx.stage.Stage
+
+import scala.util.control.NonFatal
+
 /**
   * Created by Steve on 05.01.2016.
   */
 object FileViewApp {
+  def main(args: Array[String]) {
+    Application.launch(classOf[FileViewApp], args: _*)
+  }
+}
 
+
+class FileViewApp extends javafx.application.Application {
+
+  val loader = new FXMLLoader(getClass.getResource("/fhj/swengb/project/FileViewer.fxml"))
+
+  override def start(stage: Stage): Unit =
+    try {
+      stage.setTitle("TableView Example App")
+      loader.load[Parent]()
+      stage.setScene(new Scene(loader.getRoot[Parent]))
+      stage.show()
+    } catch {
+      case NonFatal(e) => e.printStackTrace()
+    }
 }
