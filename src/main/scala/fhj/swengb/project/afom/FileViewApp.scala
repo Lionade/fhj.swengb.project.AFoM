6305837ddf2e96686597f0a5f46f3290b2905a8c
hoxhagra14 <gr_hoxha@hotmail.com>
2016-01-19 19:28:16 +0100

    - implemented imageview for images and textarea for txt-Files

--- src/main/scala/fhj/swengb/project/afom/FileViewApp.scala
+++ src/main/scala/fhj/swengb/project/afom/FileViewApp.scala
@@ -13,13 +13,15 @@ import javafx.fxml.{FXML, Initializable, FXMLLoader}
 import javafx.scene.control._
 import javafx.scene.image.{ImageView, Image}
 import javafx.scene.input.{MouseButton, ContextMenuEvent, MouseEvent}
-import javafx.scene.layout.{VBox, Pane, StackPane, BorderPane}
+import javafx.scene.layout._
 import javafx.scene.{Scene, Parent}
 import javafx.stage.Stage
 import javafx.util.Callback
+import javax.activation.MimetypesFileTypeMap
 
 
 import scala.collection.JavaConversions
+import scala.io.Source
 import scala.util.control.NonFatal
 
 
@@ -52,6 +54,8 @@ class FileViewController extends Initializable {
   import JfxUtils._
 
   @FXML var scrollpane: ScrollPane = _
+  @FXML var image: ImageView = _
+  @FXML var textfield: TextArea = _
 
   val rootItem = createNode(new File("c:/"))
   rootItem.setExpanded(true)
@@ -99,13 +103,40 @@ class FileViewController extends Initializable {
     }
   }
 
+  val tree = new TreeView[File](rootItem)
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
     //addChilds(rootItem, new File(".").listFiles())
-    val tree = new TreeView[File](rootItem)
     tree.setId("TreeView")
-
+    tree.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseClickedEvent)
     scrollpane.setContent(tree)
+  }
+
+  def mouseClickedEvent[_ >:MouseEvent] = new EventHandler[MouseEvent](){
+    def handle(event: MouseEvent): Unit ={
+      val fileDirectory: TreeItem[File] = tree.getSelectionModel.getSelectedItem
+      val fullPath: File = fileDirectory.getValue
+      val mimeType = new MimetypesFileTypeMap().getContentType(fullPath)
+      println(fileDirectory)
+      println("------------")
+      println(fileDirectory.isLeaf)
+      if (fileDirectory.isLeaf){
+        if(mimeType.substring(0,5).equalsIgnoreCase("image")){
+          image.setImage(new Image(fullPath.toURI.toString))
+          println("Ist ein image")
+        }else {
+          var text = ""
+          for (line <- Source.fromFile(fullPath).getLines) {
+            text = text + "\n" + line.toString
+          }
+          textfield.setText(text)
+          println("+++++++++")
+          //textfield.setText(lines)
+        }
+      }else {
+
+      }
+    }
   }
 
 }
