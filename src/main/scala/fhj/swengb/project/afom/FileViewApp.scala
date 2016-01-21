0d89ba69174deaa17529c25a9fae8ff6a3ac6b45
Lionade <steven95@gmx.at>
2016-01-21 14:21:02 +0100

    granit changes

--- src/main/scala/fhj/swengb/project/afom/FileViewApp.scala
+++ src/main/scala/fhj/swengb/project/afom/FileViewApp.scala
@@ -13,13 +13,15 @@ import javafx.fxml.{FXML, Initializable, FXMLLoader}
 import javafx.scene.control._
 import javafx.scene.image.{ImageView, Image}
 import javafx.scene.input.{MouseButton, ContextMenuEvent, MouseEvent}
-import javafx.scene.layout.{VBox, Pane, StackPane, BorderPane}
+import javafx.scene.layout._
 import javafx.scene.{Scene, Parent}
 import javafx.stage.Stage
 import javafx.util.Callback
+import javax.activation.MimetypesFileTypeMap
 
 
 import scala.collection.JavaConversions
+import scala.io.Source
 import scala.util.control.NonFatal
 
 
@@ -51,7 +53,9 @@ class FileViewApp extends javafx.application.Application {
 class FileViewController extends Initializable {
   import JfxUtils._
 
-  @FXML var treeView: TreeView[File] = _
+  @FXML var scrollpane: ScrollPane = _
+  @FXML var image: ImageView = _
+  @FXML var textfield: TextArea = _
 
   val rootItem = createNode(new File("c:/"))
   rootItem.setExpanded(true)
@@ -100,14 +104,53 @@ class FileViewController extends Initializable {
     }
   }
 
+  val tree = new TreeView[File](rootItem)
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
     //addChilds(rootItem, new File(".").listFiles())
-    //val tree = new TreeView[File](rootItem)
-    //tree.setId("TreeView")
-
-    treeView.setRoot(rootItem)
+    tree.setId("TreeView")
+    tree.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseClickedEvent)
+    scrollpane.setContent(tree)
   }
+
+  def mouseClickedEvent[_ >:MouseEvent] = new EventHandler[MouseEvent](){
+    def handle(event: MouseEvent): Unit ={
+      val fileDirectory: TreeItem[File] = tree.getSelectionModel.getSelectedItem
+      val fullPath: File = fileDirectory.getValue
+      if (fileDirectory.isLeaf){
+        val fileCategory  = FileCategory(fullPath)
+        fileCategory match {
+          case "image" =>
+            image.setImage(new Image(fullPath.toURI.toString))
+          case "text" =>
+            var text = ""
+            for (line <- Source.fromFile(fullPath).getLines) {
+              text = text + "\n" + line.toString
+            }
+            textfield.setText(text)
+          case _ =>
+            println("Tableview anzeigen")
+        }
+      }else {
+        println("Tableview anzeigen")
+      }
+    }
+  }
+
+  def FileCategory(file: File):String = {
+    if(textTypes.exists(file.getName.contains(_))) {
+      "text"
+    }
+    else if (imageTypes.exists(file.getName.contains(_))){
+      "image"
+    }
+    else{
+      "nothing"
+    }
+  }
+
+  lazy val textTypes: List[String] = List(".txt", ".css", "html", ".log", ".cfg", ".config", "scala", ".java" , ".html", ".xml", ".fxml", ".csv", ".xhtml", ".json", ".css", ".md")
+  lazy val imageTypes: List[String] = List (".jpg", ".png", ".ico", ".svg", ".bmp", ".gif")
 
 }
 
---
+++ src/main/scala/fhj/swengb/project/afom/TableView.scala
@@ -0,0 +1,9 @@
+package fhj.swengb.project.afom
+
+/**
+ * Created by Hoxha on 19.01.2016.
+ */
+class TableView {
+
+
+}
