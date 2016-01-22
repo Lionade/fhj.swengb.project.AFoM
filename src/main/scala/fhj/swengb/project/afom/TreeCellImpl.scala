b518e746a184badec1c6bad503f9bb9bca60f7bf
Lionade <steven95@gmx.at>
2016-01-22 16:53:26 +0100

    Merge branch 'master' of https://github.com/steinkel14/fhj.swengb.project.AFoM into steinkel14-master
    # Conflicts:
    #	src/main/scala/fhj/swengb/project/afom/FileViewApp.scala

--- src/main/scala/fhj/swengb/project/afom/TreeCellImpl.scala
+++ src/main/scala/fhj/swengb/project/afom/TreeCellImpl.scala
@@ -1,5 +1,7 @@
 package fhj.swengb.project.afom
 
+import java.io
+import java.nio.file.{Paths, Files}
 import javafx.event.EventHandler
 import javafx.scene.control.{TreeCell, TextField}
 import javafx.scene.input
@@ -8,9 +10,11 @@ import javafx.scene.input.KeyCode
 /**
   * Created by Steve on 22.01.2016.
   */
-class TreeCellImpl[File] extends TreeCell[File]{
+// TODO: Namen nicht durch vollen Path ersetzen nach bearbeitung
+class TxtFieledCell[File] extends TreeCell[File]{
   var txtField: TextField = _
 
+  // Cell wechselt auf änderbaren Zustand; start bei Doppel-klick
   override def startEdit: Unit ={
     super.startEdit()
     if(txtField == null) createTextField()
@@ -20,12 +24,14 @@ class TreeCellImpl[File] extends TreeCell[File]{
 
   }
 
+  // Cell schließt änderbaren Zustand
   override def cancelEdit: Unit = {
     super.cancelEdit
     setText(getString)
     setGraphic(getTreeItem.getGraphic)
   }
 
+  // Falls File verändert wird in TextField reinschreiben
   override def updateItem(item: File, empty: Boolean): Unit ={
     super.updateItem(item, empty)
     if(empty){
@@ -47,13 +53,18 @@ class TreeCellImpl[File] extends TreeCell[File]{
     }
   }
 
-
+  // Änderung des TreeItems bei Enter auf neuen Wert und bei Escape auf alten
   def createTextField() = {
     txtField = new TextField(getString)
     txtField.setOnKeyReleased(new EventHandler[input.KeyEvent] {
 
       override def handle(event: input.KeyEvent): Unit = {
-        if(event.getCode == KeyCode.ENTER) commitEdit(new java.io.File(txtField.getText()).asInstanceOf[File])
+        if(event.getCode == KeyCode.ENTER) {
+          val oldItem = getItem
+          commitEdit(new java.io.File(txtField.getText()).asInstanceOf[File])
+          Files.move(Paths.get(oldItem.toString), Paths.get(getItem.toString)) // Errorhandling falls von anderer Datei benützt wird
+          //FileSystemModel.move(Paths.get(oldItem.toString), Paths.get(getItem.toString)) //Überschreibung
+        }
         else if(event.getCode == KeyCode.ESCAPE) cancelEdit()
       }
     })
@@ -61,6 +72,6 @@ class TreeCellImpl[File] extends TreeCell[File]{
 
 
   def getString: String = {
-    return if(getItem == null) "" else getItem.toString
+    if(getItem == null) "" else getItem.toString
   }
 }
