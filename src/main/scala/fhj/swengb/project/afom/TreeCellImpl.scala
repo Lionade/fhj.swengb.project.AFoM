71e475fb935bf78d99cd1478befad15a5f6a1bbb
Lionade <steven95@gmx.at8rq4ewnIDs4N>
2016-01-26 17:43:53 +0100

    tried to implement Drag/Drop

--- src/main/scala/fhj/swengb/project/afom/TreeCellImpl.scala
+++ src/main/scala/fhj/swengb/project/afom/TreeCellImpl.scala
@@ -1,18 +1,57 @@
 package fhj.swengb.project.afom
 
+import java.awt.event.MouseEvent
 import java.io
 import java.nio.file.{Paths, Files}
 import javafx.event.EventHandler
-import javafx.scene.control.{TreeCell, TextField}
+import javafx.event.{EventHandler, ActionEvent}
+import javafx.scene.control.{MenuItem, ContextMenu, TreeCell, TextField}
 import javafx.scene.input
-import javafx.scene.input.KeyCode
+import javafx.scene.input.{KeyEvent, DragEvent, KeyCode}
+
 
 /**
   * Created by Steve on 22.01.2016.
   */
+object Global {
+  var zwAblage:String = "geht net"
+}
+
+
 // TODO: Namen nicht durch vollen Path ersetzen nach bearbeitung
 class TxtFieledCell[File] extends TreeCell[File]{
   var txtField: TextField = _
+  val cm: ContextMenu = new ContextMenu()
+
+
+  var menuRename = new MenuItem("Umbenennen")
+  menuRename.setOnAction(new EventHandler[ActionEvent] {
+    override def handle(event: ActionEvent): Unit = {
+      startEdit
+    }
+  })
+
+  var menuCopy = new MenuItem("Kopieren")
+  menuCopy.setOnAction(new EventHandler[ActionEvent] {
+    override def handle(event: ActionEvent): Unit = {
+      Global.zwAblage = "test"
+    }
+  })
+
+  var menuPaste = new MenuItem("Einfügen")
+  menuPaste.setOnAction(new EventHandler[ActionEvent] {
+    override def handle(event: ActionEvent): Unit = {
+      println(Global.zwAblage)
+    }
+  })
+
+  var menuCut = new MenuItem("Ausschneiden")
+  menuCut.setOnAction(new EventHandler[ActionEvent] {
+    override def handle(event: ActionEvent): Unit = println("Cut")
+  })
+
+  cm.getItems().addAll(menuRename,menuCopy,menuPaste,menuCut)
+
 
   def test: Unit = {}
 
@@ -36,7 +75,7 @@ class TxtFieledCell[File] extends TreeCell[File]{
   // Falls File verändert wird in TextField reinschreiben
   override def updateItem(item: File, empty: Boolean): Unit ={
     super.updateItem(item, empty)
-    if(empty){
+    if(empty){ // Wenn Item leer Text und Anzeige auf null setzen
       setText(null)
       setGraphic(null)
     }
@@ -51,6 +90,7 @@ class TxtFieledCell[File] extends TreeCell[File]{
       else{
         setText(getString)
         setGraphic(getTreeItem.getGraphic)
+        setContextMenu(cm)
       }
     }
   }
@@ -71,6 +111,28 @@ class TxtFieledCell[File] extends TreeCell[File]{
       }
     })
   }
+
+  setOnDragDetected(new EventHandler[input.MouseEvent] {
+    override def handle(event: input.MouseEvent): Unit = println("dragDetected")
+  })
+
+  setOnDragEntered(new EventHandler[DragEvent] {
+    override def handle(event: DragEvent): Unit = println("DragEntered")
+  })
+
+  setOnDragExited(new EventHandler[DragEvent] {
+    override def handle(event: DragEvent): Unit = println("DragExited")
+  })
+
+  setOnDragDropped(new EventHandler[DragEvent] {
+    override def handle(event: DragEvent): Unit = println("DragDropped")
+  })
+
+  setOnDragDone(new EventHandler[DragEvent] {
+    override def handle(event: DragEvent): Unit = println("DragDone")
+  })
+
+
 
 
 
